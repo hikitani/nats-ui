69d459ed7134d594952c214caddb6af4838fe8e6
hikitani <kazbekmamakaev@mail.ru>
2024-10-13 16:13:49 +0300

    feat(components): add className prop for Label

--- src/components/Label.svelte
+++ src/components/Label.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import { Label } from 'bits-ui';
+	import { cn } from '$lib/utils';
 
 	export let props: {
 		id?: string;
 		for: string;
 	};
+	export let className: string | undefined = '';
 </script>
 
-<Label.Root {...props} class="-mb-0.5 font-medium text-magnum-900">
+<Label.Root {...props} class={cn('-mb-0.5 font-medium text-magnum-900', className)}>
 	<slot />
 </Label.Root>
