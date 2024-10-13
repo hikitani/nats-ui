3d4565ee4a7cf41de9db94be6043248b4343a597
hikitani <kazbekmamakaev@mail.ru>
2024-10-13 16:14:46 +0300

    feat(components): add problems for Form

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -4,6 +4,7 @@
 		type: InputType;
 		label: string;
 		label_inline?: boolean;
+		disabled?: boolean;
 
 		text?: TextOptions;
 		number?: NumberOptions;
@@ -11,10 +12,48 @@
 		password?: PasswordOptions;
 	};
 
-	export type Data = { [key: string]: any };
+	export type SendOpions = {
+		title?: string;
+		redirectRef?: string;
+		onSend: (data: Data) => Promise<Problems | void>;
+	};
+
+	export type Data = { [id: string]: any };
+
+	export type Problem = {
+		message: string;
+	};
+
+	export class Problems {
+		private problems: {
+			[id: string]: Problem[];
+		};
+
+		constructor() {
+			this.problems = {};
+		}
+
+		add(inputID: string, problem: Problem): Problems {
+			if (!(inputID in this.problems)) {
+				this.problems[inputID] = [];
+			}
+
+			this.problems[inputID].push(problem);
+			return this;
+		}
+
+		get(inputID: string): Problem[] {
+			return this.problems[inputID];
+		}
+
+		notEmpty(): boolean {
+			return Object.keys(this.problems).length !== 0;
+		}
+	}
 </script>
 
 <script lang="ts">
+	import { makeid } from '$lib/utils';
 	import Button from './Button.svelte';
 	import Input, {
 		type EmailOptions,
@@ -25,11 +64,32 @@
 	import { InputType } from './Input.svelte';
 	import Label from './Label.svelte';
 
+	const defaultSend: SendOpions = {
+		title: 'Send',
+		onSend: async () => {}
+	};
+	const defaultCloseTitle = 'Close';
+
 	export let title: string;
-	export let send_title: string = 'Send';
-	export let close_title: string = 'Close';
+	export let close_title: string = defaultCloseTitle;
 	export let inputs: InputInfo[];
 	export let data: Data = {};
+	export let send: SendOpions;
+	send = { ...defaultSend, ...send };
+
+	const formId = makeid(6);
+
+	// const validateData
+	const onClick = async (event: MouseEvent) => {
+		const problems = await send.onSend(data);
+		if (problems && Object.keys(problems).length !== 0) {
+			currentProblems = problems;
+		} else if (send.redirectRef) {
+			document.location = send.redirectRef;
+		}
+	};
+
+	let currentProblems = new Problems();
 </script>
 
 <div
@@ -47,24 +107,41 @@
 		<!-- Inputs -->
 		<div class="flex flex-col">
 			{#each inputs as item (item.id)}
+				{@const inputId = `${formId}-input-${item.id}`}
 				<div class="flex py-1 {item.label_inline ? 'flex-row items-center py-2' : 'flex-col'}">
 					<div class={item.label_inline ? 'pr-4' : ''}>
-						<Label props={{ for: item.id }}>{item.label}</Label>
+						<Label props={{ for: inputId }}>{item.label}</Label>
 					</div>
 					<Input
+						id={inputId}
 						type={item.type}
+						disabled={item.disabled}
 						text={item.text}
 						email={item.email}
 						number={item.number}
 						password={item.password}
 						bind:value={data[item.id]}
 					/>
+					{#if currentProblems.get(item.id)}
+						{@const problems = currentProblems.get(item.id)}
+						<ul
+							class="flex list-outside list-disc flex-col gap-y-0 pl-4 text-left font-mono text-sm leading-4 tracking-tight"
+						>
+							{#each problems as problem}
+								<li class="text-zinc-700">
+									<Label className="text-rose-600 font-semibold" props={{ for: inputId }}
+										>{problem.message}</Label
+									>
+								</li>
+							{/each}
+						</ul>
+					{/if}
 				</div>
 			{/each}
 			<div class="flex flex-row pt-10">
 				<Button red goback>{close_title}</Button>
 				<span class="grow"></span>
-				<Button>{send_title}</Button>
+				<Button {onClick}>{send.title}</Button>
 			</div>
 		</div>
 	</div>
