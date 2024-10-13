045493b5a5beaed722a66606a387862ee5bac539
hikitani <kazbekmamakaev@mail.ru>
2024-10-13 16:15:42 +0300

    feat(components): add for Button prop to go back when clicked

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -2,20 +2,30 @@
 	import { Button } from 'bits-ui';
 	import { twJoin } from 'tailwind-merge';
 
+	export let href: string = '';
+
+	/** If true, the button will call history.back() when clicked */
+	export let goback: boolean = false;
+	export let onClick: undefined | ((event: MouseEvent) => void) = undefined;
 	export let red: boolean = false;
 
 	const btnCommon = 'relative rounded px-4 py-2 font-bold text-black';
 	const btnPrimary = 'bg-magnum-300 hover:bg-magnum-600 hover:text-white active:bg-magnum-800';
 	const btnRed = 'bg-rose-300 hover:bg-rose-600 hover:text-white active:bg-rose-800';
 
-	export let href: string = '';
-	export let goback: boolean = false;
+	const clickHandler = (event: MouseEvent) => {
+		if (onClick) {
+			onClick(event);
+		}
 
-	const gobackHandler = (event: MouseEvent) => window.history.back();
+		if (goback) {
+			window.history.back();
+		}
+	};
 </script>
 
 <Button.Root
-	on:click={goback ? gobackHandler : null}
+	on:click={clickHandler}
 	href={goback ? '' : href}
 	class={twJoin(btnCommon, red ? btnRed : btnPrimary)}
 >
