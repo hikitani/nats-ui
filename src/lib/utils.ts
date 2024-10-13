6923f708c18b293ddb726cd883e312842f7cd296
hikitani <kazbekmamakaev@mail.ru>
2024-10-13 16:12:04 +0300

    feat: add makeid func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -52,3 +52,16 @@ export function flyAndScale(node: Element, params?: FlyAndScaleParams): Transiti
 		easing: cubicOut
 	};
 }
+
+export function makeid(length: number) {
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	const charactersLength = characters.length;
+
+	let result = '';
+	let counter = 0;
+	while (counter < length) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		counter += 1;
+	}
+	return result;
+}
