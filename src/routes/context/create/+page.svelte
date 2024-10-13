52628895b97f7231bb3ed0deb59ba4c28066f1e4
hikitani <kazbekmamakaev@mail.ru>
2024-10-13 16:22:10 +0300

    feat(context): add create page

--- src/routes/context/create/+page.svelte
+++ src/routes/context/create/+page.svelte
@@ -1,10 +1,67 @@
 <script lang="ts">
 	import { InputType } from '@components/Input.svelte';
-	import Form, { type InputInfo } from '@components/Form.svelte';
+	import Form, { Problems, type InputInfo, type SendOpions } from '@components/Form.svelte';
+	import { ContextServer } from '@tables/context';
+	import { castTable } from '@tables/utils';
+	import { db } from '@db';
+	import { pushState } from '$app/navigation';
 
-	let context: {
-		name: string;
-		server: string;
+	let send: SendOpions = {
+		title: 'Create',
+		redirectRef: '/',
+		async onSend(data) {
+			const context = castTable(data, ContextServer);
+			if (!context) {
+				pushState('', {
+					status: 500,
+					message: 'Cannot create context, try again'
+				});
+				return;
+			}
+
+			let problems = new Problems();
+
+			if (context.name === '') {
+				problems.add('name', { message: 'Required' });
+			}
+
+			if (context.server === '') {
+				problems.add('server', { message: 'Required' });
+			}
+
+			if (!context.name.match(/^[\w-]+$/)) {
+				problems.add('name', { message: 'Allowed only a-z A-Z and symbols -_' });
+			}
+
+			try {
+				let url = new URL(context.server);
+				const urlPathAndQuery = url.pathname + url.search + url.hash;
+				if (!['', '/'].includes(urlPathAndQuery)) {
+					problems.add('server', {
+						message: `Specify only url origin without url path, query (got ${urlPathAndQuery})`
+					});
+				}
+
+				if (!['http:', 'https:'].includes(url.protocol)) {
+					problems.add('server', {
+						message: `Allowed only protocol http or https, not ${url.protocol}`
+					});
+				}
+			} catch (TypeError) {
+				problems.add('server', { message: 'Server must be valid URL' });
+			}
+
+			if (problems.notEmpty()) {
+				return problems;
+			}
+
+			try {
+				await db.contexts.add(context);
+			} catch {
+				problems.add('name', { message: 'This name already exists' });
+				return problems;
+			}
+		}
 	};
 
 	let inputs: InputInfo[] = [
@@ -19,14 +76,14 @@
 		{
 			id: 'server',
 			type: InputType.Text,
-			label: 'Server',
+			label: 'Server URL',
 			text: {
-				placeholder: 'nats://example.com:4222'
+				placeholder: 'https://example.com:8222'
 			}
 		}
 	];
 </script>
 
 <div class="flex min-h-screen items-center justify-center">
-	<Form title="New Context" {inputs} bind:data={context} send_title="Create" />
+	<Form title="New Context" {inputs} {send} />
 </div>
